6ee7aca9dff223b11db050d5a05cae75002816f2
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-08-27 22:06:10 +0100

    loyalty discount tested for non members - passed

--- deal_management_specs/LoyaltyDiscountTest.java
+++ deal_management_specs/LoyaltyDiscountTest.java
@@ -7,6 +7,7 @@ import deal_management.*;
 public class LoyaltyDiscountTest {
 
   ShoppingBasket basket;
+  ShoppingBasket basket2;
   Food food1;
   Food food2;
   Food food3;
@@ -17,11 +18,14 @@ public class LoyaltyDiscountTest {
   TenPercentDiscount tenPercent;
   LoyaltyDiscount loyalty;
   Customer customer;
+  Customer customer2;
 
   @Before
   public void before() {
     customer = new Customer("Chris", "Donnelly", true);
+    customer2 = new Customer("David", "Bowie", false);
     basket = new ShoppingBasket(customer);
+    basket2 = new ShoppingBasket(customer2);
     food1 = new Food("Apple", "Fruit", 0.80);
     food2 = new Food("Steak", "Meat", 6.50);
     food3 = new Food("Steak", "Meat", 6.50);
@@ -43,7 +47,7 @@ public class LoyaltyDiscountTest {
   }
 
   @Test
-  public void canApplyLoyaltyDiscountToComplexBasket(){
+  public void canApplyLoyaltyDiscountToComplexBasket() {
     basket.addItemToBasket(wine1);
     basket.addItemToBasket(wine2);
     basket.addItemToBasket(wine3);
@@ -56,6 +60,22 @@ public class LoyaltyDiscountTest {
     tenPercent.calculateDiscount(basket);
     loyalty.calculateDiscount(basket, customer);
     assertEquals(20.99, basket.getTotal(), 0.1);
+  }
+
+  @Test
+  public void noDiscountForNonLoyaltyCardHolders() {
+    basket2.addItemToBasket(wine1);
+    basket2.addItemToBasket(wine2);
+    basket2.addItemToBasket(wine3);
+    basket2.addItemToBasket(food1);
+    basket2.addItemToBasket(food2);
+    basket2.addItemToBasket(food3);
+    basket2.updateTotal();
+    bogof1.calculateDiscount(basket2);
+    bogof1.applyDiscount(basket2);
+    tenPercent.calculateDiscount(basket2);
+    loyalty.calculateDiscount(basket2, customer2);
+    assertEquals(21.42, basket2.getTotal(), 0.1);
   }
 
 
