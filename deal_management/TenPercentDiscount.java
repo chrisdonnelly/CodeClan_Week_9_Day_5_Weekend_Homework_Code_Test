6fa85e983f9432fd78de4afe4de1b227bfce44e2
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-08-27 21:22:46 +0100

    10 percent discount test added and passed

---
+++ deal_management/TenPercentDiscount.java
@@ -0,0 +1,16 @@
+package deal_management;
+import item_management.*;
+import shop_management.*;
+import behaviours.*;
+
+public class TenPercentDiscount implements Discounts {
+
+  public double calculateDiscount(ShoppingBasket basket) {
+    double discount = 0;
+    if (basket.total >= 20.00) {
+      discount = basket.total * 0.1;
+    }
+    return basket.total -= discount;
+  }
+
+}
---
+++ deal_management_specs/TenPercentDiscountTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import shop_management.*;
+import item_management.*;
+import deal_management.*;
+
+public class TenPercentDiscountTest {
+
+  ShoppingBasket basket;
+  Food food1;
+  Food food2;
+  Food food3;
+  Wine wine1;
+  Wine wine2;
+  Wine wine3;
+  Bogof bogof1;
+  TenPercentDiscount tenPercent;
+
+  @Before
+  public void before() {
+    basket = new ShoppingBasket();
+    food1 = new Food("Apple", "Fruit", 0.80);
+    food2 = new Food("Steak", "Meat", 6.50);
+    food3 = new Food("Steak", "Meat", 6.50);
+    wine1 = new Wine("Emulsion Remover", "Shiraz", 5.00);
+    wine2 = new Wine("Grape Posion", "Shiraz", 5.00);
+    wine3 = new Wine("Lovely Juice", "Shiraz", 5.00);
+    bogof1 = new Bogof("Shiraz");
+    tenPercent = new TenPercentDiscount();
+  }
+
+  @Test
+  public void canApplyTenPercentDiscountToBasket() {
+    basket.addItemToBasket(wine1);
+    basket.addItemToBasket(wine2);
+    basket.addItemToBasket(wine3);
+    basket.addItemToBasket(food1);
+    basket.addItemToBasket(food2);
+    basket.addItemToBasket(food3);
+    basket.updateTotal();
+    bogof1.calculateDiscount(basket);
+    bogof1.applyDiscount(basket);
+    tenPercent.calculateDiscount(basket);
+    assertEquals(21.42, basket.getTotal(), 0.1);
+  }
+
+}
+
