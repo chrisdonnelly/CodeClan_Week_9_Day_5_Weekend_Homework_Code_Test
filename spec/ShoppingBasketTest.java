882619e57de3a2749a7d3efc5620c288d6c070a3
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-08-27 16:10:23 +0100

    First test initialise created - 1 fail

--- spec/ShoppingBasketTest.java
+++ spec/ShoppingBasketTest.java
@@ -1,5 +1,17 @@
 import org.junit.*;
 
 public class ShoppingBasketTest {
+
+  ShoppingBasket basket;
+
+  @Before
+  public void before() {
+    basket = new ShoppingBasket;
+  }
+
+  @Test
+  public void canCreateNewBasket() {
+    assertEquals([], basket.contents)
+  }
   
 }
